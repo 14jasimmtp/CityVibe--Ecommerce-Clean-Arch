5a14a27ca83a23c9e0db7426efb3a2f9e539db80
Muhamed Jasim <jasimmtp84@gmail.com>
2024-02-01 10:39:40 +0530

    added redirection route

--- pkg/api/server.go
+++ pkg/api/server.go
@@ -1,6 +1,8 @@
 package server
 
 import (
+	"net/http"
+
 	handlers "github.com/14jasimmtp/CityVibe-Project-Clean-Architecture/pkg/api/handler"
 	middlewares "github.com/14jasimmtp/CityVibe-Project-Clean-Architecture/pkg/api/middleware"
 	"github.com/14jasimmtp/CityVibe-Project-Clean-Architecture/pkg/routes"
@@ -26,13 +28,15 @@ func StartServer(
 
 	engine.Use(gin.Logger())
 
-
 	engine.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	engine.LoadHTMLFiles("/home/jasim/CityVibe-Ecommerce-CleanCode-Project/template/*")
 
-	routes.AdminRoutes(engine, admin, product, category, order, coupon,adminMiddleware)
-	routes.UserRoutes(engine, user, product, admin, wishlist, cart, order, payment, coupon,userMiddleware)
-
+	routes.AdminRoutes(engine, admin, product, category, order, coupon, adminMiddleware)
+	routes.UserRoutes(engine, user, product, admin, wishlist, cart, order, payment, coupon, userMiddleware)
+	//redirect
+	engine.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusFound, "/swagger/index.html")
+	})
 	engine.Run(":3000")
 
 }
